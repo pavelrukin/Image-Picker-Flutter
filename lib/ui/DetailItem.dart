762bcb54d31be2b2f693dd2b669f69e69e786525
Pavel Rukin <rivelr@gmail.com>
2020-03-06 14:07:29 +0200

    update DetailList with sliver

--- lib/ui/DetailItem.dart
+++ lib/ui/DetailItem.dart
@@ -13,60 +13,68 @@ class DetailItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Image.network(dataResponse.urls.small,
-        width: 600, height: 240, fit: BoxFit.cover,),titleSection , textSection],
-    ));
-  }
-
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+        body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          pinned: true,
+          floating: false,
+          expandedHeight: 256,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Image.network(
+                  dataResponse.urls.small,
+                  fit: BoxFit.cover,
+                ),
+                const DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment(0.0, -1.0),
+                            end: Alignment(0.0, -0.4),
+                            colors: <Color>[
+                      Color(0x60000000),
+                      Color(0x0000000)
+                    ]))),
+                Positioned(
+                  bottom: 20,
+                  left: 0,
+                  child: Container(
+                    color: Color.fromRGBO(255, 255, 255, 0.5),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.assignment_ind,
+                          size: 40,
+                        ),
+                        Text(dataResponse.user.name)
+                      ],
+                    ),
                   ),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
-        /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
+        SliverGrid(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 4.0,
+          ),
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                color: Colors.teal[100 * (index % 9)],
+                child: Text('${dataResponse.description} $index'),
+              );
+            },
+            childCount: 100,
+          ),
         ),
-        Text('41'),
       ],
-    ),
-  );
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
-      softWrap: true,
-    ),
-  );
+    ));
+  }
 }
