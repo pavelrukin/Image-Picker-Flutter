c1acfab0baf115c565927311867da61c026fd2b4
Pavel Rukin <rivelr@gmail.com>
2020-03-20 17:45:23 +0200

    add dB

--- lib/ui/ListFavorite.dart
+++ lib/ui/ListFavorite.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_image_picker/db/DBProvider.dart';
+import 'package:flutter_test_image_picker/model/DataResponse.dart';
 
 class ListFavorite extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _ListFavoriteState();
 }
-
+int id;
 
 
 
 class _ListFavoriteState extends State<ListFavorite> {
 
+  List<DataResponse> _dataResponse = List<DataResponse>();
 
+  DBProvider _dbProvider ;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _dbProvider.initDB();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("FAVORITE LIST"),
-      ),
-
+    return Scaffold(
+      appBar: AppBar(title: Text("Favorite"),),
+      body: ListView.builder(
+        itemCount:  _dataResponse.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              title: Text(_dataResponse[index].user.name),
+            );
+          }),
     );
   }
 
